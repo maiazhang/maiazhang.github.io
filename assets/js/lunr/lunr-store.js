0747350e3e42fc2c6acdc96937036b4dc77556ca
maiazhang <maiazhang@users.noreply.github.com>
2025-09-12 15:15:03 +0000

    deploy: 8ef4e79135e4cf33040aa4827dc18db1ac73c461

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "Earthquake Clustering in the Japan Subduction Zone",
+        "excerpt":"I am looking at earthquake clustering in one of the most complete earthquake catalogs in the world, the Japan Meteorological Agency Catalog (JMAC). I apply a nearest-neighbors clustering method developed by Zaliapin &amp; Ben-Zion , looking to scale the approach from a point-source model (which cannot be reliably assumed for...","categories": ["Blog"],
+        "tags": [],
+        "url": "/blog/post-jpnclust/",
+        "teaser": null
+      },{
+        "title": "Structure and Near-source Behavior of a Highly Active Aftershock Sequence",
+        "excerpt":"I present a high-resolution near-source catalog for the aftershock sequence of the 2020 \\(M_w\\) 6.5 Monte Cristo Range Earthquake (MCRE) built from a to-date untapped nodal deployment. This catalog not only illuminates previously unobserved structure and spatiotemporal evolution of the aftershock sequnece, it also primes us to explore near-source properties...","categories": ["Blog"],
+        "tags": [],
+        "url": "/blog/post-mcre/",
+        "teaser": null
+      }]
